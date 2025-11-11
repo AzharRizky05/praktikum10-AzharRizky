ae98cb5ab619faae86583b1bb9612bd98644a3c9
AzharRizky05 <trinandaazharrizky@gmail.com>
2025-11-11 13:10:14 +0700

    Update: tambah script koneksi database di KoneksiDatabase

--- koneksi.php
+++ koneksi.php
@@ -1,1 +1,13 @@
-<?php\n// koneksi database (contoh)\n$host='localhost'; $user='root'; $pass=''; $db='db_praktikum';\n?>
+<?php
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db   = 'db_praktikum';
+
+$koneksi = mysqli_connect($host, $user, $pass, $db);
+
+    echo 'Koneksi gagal!';
+} else {
+    echo 'Koneksi berhasil!';
+}
+?>
